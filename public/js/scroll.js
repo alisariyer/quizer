c7b08c9656dbd10a79812e6684543ac3ced382d5
Ali Sariyer <alisariyer@gmail.com>
2023-04-28 18:08:23 +0200

    Add menu-toggle and functionality

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,14 +1,34 @@
-const navbar = document.querySelector(".navbar-scroll");
-document.addEventListener("DOMContentLoaded", (e) => {
+window.addEventListener("DOMContentLoaded", (e) => {
+  const navbar = document.querySelector(".navbar-scroll");
+  const btnMenus = document.querySelectorAll(".btn-menu");
+  const menuToggle = document.querySelector(".menu-toggle");
+  let isMenuToggled = false;
+
+  btnMenus.forEach((btn) =>
+    btn.addEventListener("click", () => {
+      console.log(isMenuToggled);
+      if (isMenuToggled) {
+        menuToggle.classList.add("d-none");
+        isMenuToggled = !isMenuToggled;
+      } else {
+        menuToggle.classList.remove("d-none");
+        isMenuToggled = !isMenuToggled;
+      }
+    })
+  );
+
   document.addEventListener("scroll", (e) => {
     console.log("window.innerHeight: ", window.innerHeight);
     console.log("window.scrollY: ", window.scrollY);
     console.log("document.body.offsetTop", document.body.offsetTop);
     console.log("document.body.offsetHeight", document.body.offsetHeight);
     if (window.scrollY > 60) {
-      navbar.classList.remove("hidden");
+      navbar.classList.remove("d-none");
+      menuToggle.style.top = `${window.scrollY + 60}px`;
     } else {
-      navbar.classList.add("hidden");
+      navbar.classList.add("d-none");
+      menuToggle.style.top = `${64}px`;
     }
   });
 });
+
