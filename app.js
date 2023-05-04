19e62b87bb410f18aeede01e75c251fb2c73905c
Ali Sariyer <alisariyer@gmail.com>
2023-05-04 20:45:47 +0200

    Add basic login system

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const morgan = require("morgan");
 const fs = require("fs");
 const mongoose = require("mongoose");
 const { model } = require("mongoose");
-const questionSchema = require("./model/quiz")
+const questionSchema = require("./model/quiz");
+const { parseArgs } = require("util");
 
 // Establish MongoDB Connection
 const main = async () => {
@@ -31,24 +32,54 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // setup morgan logger
-const accessLogStream = fs.WriteStream(path.join(__dirname, "access.log"), {
-  flags: "a",
-});
-app.use(morgan("tiny", { stream: accessLogStream }));
+// const accessLogStream = fs.WriteStream(path.join(__dirname, "access.log"), {
+//   flags: "a",
+// });
+// app.use(morgan("tiny", { stream: accessLogStream }));
+app.use(morgan("tiny"));
+
+let isLoggedIn = false;
+const EMAIL = 'test@gmail.com';
+const PASSWORD = 'admin.123';
+
+const login = (req, res, next) => {
+  if (isLoggedIn) return next();
+  // 303: Redirect for undefined reason
+  return res.redirect(303, '/login');
+}
 
 // GET home route
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (email === EMAIL && password === PASSWORD) {
+    isLoggedIn = true;
+    return res.redirect(302, '/');
+  }
+  // 401 Unauthorized
+  res.status(401).send('Wrong credentials!!!');
+})
+
+app.get('/logout', (req, res) => {
+  isLoggedIn = false;
+  res.redirect('/');
+})
+
 // GET questions (send all questions to client side from DB)
-app.get("/quiz", async (req, res) => {
+app.get("/quiz", login, async (req, res) => {
   const questions = await Question.find({})
   res.render("quiz", { questions });
 });
 
 // POST answers (and send back with correct answers to client side)
-app.post("/quiz", async (req, res) => {
+app.post("/quiz", login, async (req, res) => {
   const questions = await Question.find({});
   const answers = req.body;
   questions.forEach((question) => {
@@ -63,12 +94,12 @@ app.post("/quiz", async (req, res) => {
 });
 
 // GET new question
-app.get("/questions/new", (req, res) => {
+app.get("/questions/new", login, (req, res) => {
   res.render("new");
 });
 
 // POST new question (and save in DB)
-app.post("/questions/new", async (req, res) => {
+app.post("/questions/new", login, async (req, res) => {
   const { question, answers, correct } = req.body;
   const newQuestion = new Question({
     id: uuidv4(),
@@ -81,13 +112,13 @@ app.post("/questions/new", async (req, res) => {
 });
 
 // GET questions (send all questions to show as a list)
-app.get("/questions", async (req, res) => {
+app.get("/questions", login, async (req, res) => {
   const questions = await Question.find({});
   res.render("questions", { questions });
 });
 
 // GET a specific question
-app.get("/questions/:id", async (req, res) => {
+app.get("/questions/:id", login, async (req, res) => {
   const { id } = req.params;
   if (id) {
     const question = await Question.findOne({ id });
@@ -97,7 +128,7 @@ app.get("/questions/:id", async (req, res) => {
 });
 
 // UPDATE a specific question
-app.put("/questions/:id", async (req, res) => {
+app.put("/questions/:id", login, async (req, res) => {
   const { id } = req.params;
   const { question, answers, correct } = req.body;
   if (id) {
@@ -108,7 +139,7 @@ app.put("/questions/:id", async (req, res) => {
 });
 
 // DELETE a specific question
-app.delete("/questions/:id", async (req, res) => {
+app.delete("/questions/:id", login, async (req, res) => {
   const { id } = req.params;
   if (id) {
     await Question.deleteOne({ id })
@@ -116,7 +147,10 @@ app.delete("/questions/:id", async (req, res) => {
   res.redirect("/questions");
 });
 
+app.all('*', login);
+
 app.use((err, req, res, next) => {
+  console.log('error 404.....');
   const { status = 500, message = "Something went wrong" } = err;
   res.status(status).send(message);
 });
