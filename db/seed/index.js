7c466169940c67f5d2e4ea4c17ebdc89e5165b8a
Ali Sariyer <alisariyer@gmail.com>
2023-05-07 00:00:57 +0200

    Clean up seed db file

--- db/seed/index.js
+++ db/seed/index.js
@@ -1,56 +1,26 @@
 const mongoose = require("mongoose");
-const { Schema, model } = require("mongoose");
+const Question = require('../models/question');
 const questions = require("./questions");
+require('dotenv').config();
 
 // Establish MongoDB Connection
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
 const main = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/quiz");
+  await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 };
 
 main()
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
-// Create a new Schema for quiz
-const questionSchema = new Schema({
-  id: {
-    type: String,
-    required: true,
-  },
-  question: {
-    type: String,
-    required: [true, "Question must not be empty!"],
-    trim: true,
-  },
-  answers: {
-    type: [String],
-    validate: {
-      validator: function (v) {
-        return v.length > 3;
-      },
-      message: () => `Must be 4 answers at least for the question!`,
-    },
-    trim: true,
-  },
-  correct: {
-    type: Number,
-    default: 0,
-    enum: {
-      values: [0, 1, 2, 3],
-      message: "The correct answer must be between 0-3, {VALUE} is not valid!",
-    },
-    required: true,
-    trim: true,
-  },
-});
-
-const Question = model("Question", questionSchema);
-
-async function cleanup() {
+async function seedDB() {
   await Question.deleteMany({});
   await Question.insertMany(questions);
   mongoose.connection.close();
+  console.log('Seed DB process is completed.');
   process.exit(0);
 }
 
-cleanup();
+seedDB();
