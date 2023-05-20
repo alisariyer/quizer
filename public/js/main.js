5fd46780bfe1b1f52519829c0e25ad830b4886e3
Ali Sariyer <alisariyer@gmail.com>
2023-05-20 11:56:50 +0200

    Hide automatically info flash in main js

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,15 @@
 const closeIcons = document.querySelectorAll('.close-icon');
 const modals = document.querySelectorAll('.modal');
 
+const hideModals = () => {
+    modals.forEach(modal => modal.classList.add('d-none'));
+}
+
 closeIcons.forEach(closeIcon => {
-    closeIcon.addEventListener('click', (e) => {
-        modals.forEach(modal => modal.classList.add('d-none'));
-    })
+    closeIcon.addEventListener('click', hideModals)
+});
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    // if a modal is shown, hide it in 1500 ms
+    if (modals && modals.length) setTimeout(hideModals, 1500);
 })
